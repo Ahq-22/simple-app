591bdfff4906a6017665885bf74ec52af7e4abab
Ahq-22 <103901612+Ahq-22@users.noreply.github.com>
2023-08-07 16:25:24 +0300

    init

---
+++ proplemSolving.cs
@@ -0,0 +1,20 @@
+using System;
+
+public class program
+{
+    public static int multipying(int num1)
+    {
+        if (num1 % 2 == 0)
+        {
+            return num1 * 8;
+        }
+        else
+        {
+            return num1 * 9
+        }
+    }
+    public static void Main(string[] args)
+    {
+        multipying(4);
+    }
+}
